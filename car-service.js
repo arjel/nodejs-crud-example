76c9379618d911a7d57908c640974b8c6be370da
arjel <arjel.dragoj@gmail.com>
2019-10-18 17:06:39 +0200

    Formats code.

--- car-service.js
+++ car-service.js
@@ -1,52 +1,55 @@
-const util = require('util');
-const eventEmitter = require('events');
+const util = require("util");
+const eventEmitter = require("events");
 
-// const promise = require('bluebird');
-const AppDAO = require('./dao');
-const CarRepository = require('./car-repository');
+const AppDAO = require("./dao");
+const CarRepository = require("./car-repository");
 
-const dao = new AppDAO('./database.sqlite3');
+const dao = new AppDAO("./database.sqlite3");
 const repo = new CarRepository(dao);
 
-function CarService(){
-	eventEmitter.call(this);
+function CarService() {
+    eventEmitter.call(this);
 }
 util.inherits(CarService, eventEmitter);
 
-CarService.prototype.create = function(body){
-	var car = body;
-	repo.createTable()
-		.then(() => repo.create(car.brand, car.model, car.plate, car.owner))
-		.then(() => repo.getByPlate(car.plate))
-		.then((car) => {
-			this.emit('createEnd', car);
-	
-		}).catch((err) => {
-			this.emit('error', err);
-		});
-}
+CarService.prototype.create = function(body) {
+    var car = body;
+    repo.createTable()
+        .then(() => repo.create(car.brand, car.model, car.plate, car.owner))
+        .then(() => repo.getByPlate(car.plate))
+        .then(row => {
+            this.emit("createEnd", row);
+        })
+        .catch(err => {
+            this.emit("createError", err);
+        });
+};
 
-CarService.prototype.getAll = function(){
-	repo.getAll().then((rows) => {
-		this.emit('getAllEnd', rows);
-		
-	}).catch((err) => {
-		this.emit('error', err);					
-	});	
-}
+CarService.prototype.getAll = function() {
+    repo.getAll()
+        .then(rows => {
+            this.emit("getAllEnd", rows);
+        })
+        .catch(err => {
+            this.emit("getAllError", err);
+        });
+};
 
-CarService.prototype.getByPlate = function(plate){	
-	repo.getByPlate(plate).then((row) => {
-		if(row){
-			this.emit('getByPlateEnd', row);
-		
-		} else {
-			this.emit('getByPlateNotFound', 'car not found for plate: "' + plate + '"');					
-		}
-		
-	}).catch((err) => {
-		this.emit('error', err);					
-	});	
-}
+CarService.prototype.getByPlate = function(plate) {
+    repo.getByPlate(plate)
+        .then(row => {
+            if (row) {
+                this.emit("getByPlateEnd", row);
+            } else {
+                this.emit(
+                    "getByPlateNotFound",
+                    'car not found for plate: "' + plate + '"'
+                );
+            }
+        })
+        .catch(err => {
+            this.emit("getByPlateError", err);
+        });
+};
 
-module.exports = CarService
+module.exports = CarService;
